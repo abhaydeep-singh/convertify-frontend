11e18e4cee29750007157a3f15ff7a3b5766aacf
abhay <abhaydeep7890@gmail.com>
2025-04-11 14:11:38 +0530

    fixed button tsx issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, ChangeEvent } from "react";
 import axios from "axios";
-import { Button } from "./components/ui/button";
+
 import { Progress } from "@/components/ui/progress";
 import {
   Select,
